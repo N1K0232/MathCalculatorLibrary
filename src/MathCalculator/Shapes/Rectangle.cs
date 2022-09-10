58dd8343b79584b3c7a852e34a30f2026ecf76a3
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-09-10 21:08:30 +0200

    created Rectangle and Square shapes

---
+++ src/MathCalculator/Shapes/Rectangle.cs
@@ -0,0 +1,66 @@
+﻿using MathCalculator.Common;
+
+namespace MathCalculator.Shapes;
+
+public class Rectangle : Shape
+{
+    private float width;
+    private float height;
+
+    public Rectangle()
+    {
+        width = 0;
+        height = 0;
+    }
+
+    public float Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            width = value;
+        }
+    }
+
+    public float Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            height = value;
+        }
+    }
+
+    public override float Area
+    {
+        get
+        {
+            return width * height;
+        }
+    }
+
+    public override float Perimeter
+    {
+        get
+        {
+            return (width + height) * 2;
+        }
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && !IsDisposed)
+        {
+            width = 0;
+            height = 0;
+        }
+
+        base.Dispose(disposing);
+    }
+}
---
+++ src/MathCalculator/Shapes/Square.cs
@@ -0,0 +1,51 @@
+﻿using MathCalculator.Common;
+
+namespace MathCalculator.Shapes;
+
+public class Square : Shape
+{
+    private float side;
+
+    public Square()
+    {
+        side = 0;
+    }
+
+    public float Side
+    {
+        get
+        {
+            return side;
+        }
+        set
+        {
+            side = value;
+        }
+    }
+
+    public override float Area
+    {
+        get
+        {
+            return side * side;
+        }
+    }
+
+    public override float Perimeter
+    {
+        get
+        {
+            return side * 4;
+        }
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && !IsDisposed)
+        {
+            side = 0;
+        }
+
+        base.Dispose(disposing);
+    }
+}
