29964f181761006a834fd77d5ba64f1263b0b07b
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-09-10 21:08:18 +0200

    created abstract class for Shapes

---
+++ src/MathCalculator/Common/Shape.cs
@@ -0,0 +1,68 @@
+﻿namespace MathCalculator.Common;
+
+public abstract partial class Shape : ICloneable, IDisposable
+{
+    private string name = string.Empty;
+    private bool disposed = false;
+
+    protected Shape()
+    {
+    }
+
+    ~Shape()
+    {
+        Dispose(disposing: false);
+    }
+
+
+    public abstract float Area { get; }
+
+    public abstract float Perimeter { get; }
+
+    public string Name
+    {
+        get
+        {
+            ThrowIfDisposed();
+            return name;
+        }
+        set
+        {
+            ThrowIfDisposed();
+            name = value;
+        }
+    }
+
+    internal bool IsDisposed => disposed;
+
+    public virtual Shape? Clone()
+    {
+        ICloneable cloneable = this;
+        object? clone = cloneable.Clone();
+        return clone as Shape;
+    }
+    object ICloneable.Clone()
+    {
+        return this;
+    }
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            disposed = true;
+        }
+    }
+    internal void ThrowIfDisposed()
+    {
+        if (disposed)
+        {
+            Type currentType = GetType();
+            throw new ObjectDisposedException(currentType.FullName);
+        }
+    }
+}
