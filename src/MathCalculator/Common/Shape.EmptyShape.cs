b0d1f2c9d91a45056dce6d65b9fd1c7e4d57831c
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-09-11 11:16:52 +0200

    added EmptyShape class

---
+++ src/MathCalculator/Common/Shape.EmptyShape.cs
@@ -0,0 +1,15 @@
+﻿namespace MathCalculator.Common;
+
+public abstract partial class Shape
+{
+    private sealed class EmptyShape : Shape
+    {
+        internal EmptyShape()
+        {
+            Name = "Empty Shape";
+        }
+
+        public override float Area => 0;
+        public override float Perimeter => 0;
+    }
+}
