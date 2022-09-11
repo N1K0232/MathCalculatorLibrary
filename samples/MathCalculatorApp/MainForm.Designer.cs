fb134cfa28a05f715e790d43df66b72acc5e388b
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-09-11 10:14:08 +0200

    added sample project

---
+++ samples/MathCalculatorApp/MainForm.Designer.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace MathCalculatorApp;
+
+public partial class MainForm : Form
+{
+    private IContainer components = null;
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+        components = new Container();
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Text = "Math Calculator Sample Application";
+        Load += new EventHandler(Form_Load);
+    }
+}
