31bf9e407b8ea54dd8767264bf69ba9783ada69a
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-09-13 20:52:03 +0200

    added Parallelogram and Rhombus shapes

---
+++ src/MathCalculator/Shapes/Parallelogram.cs
@@ -0,0 +1,27 @@
+﻿using MathCalculator.Common;
+
+namespace MathCalculator.Shapes;
+
+public class Parallelogram : Shape
+{
+    private float longSide;
+    private float shortSide;
+
+    public Parallelogram()
+    {
+    }
+
+    public override float Area => throw new NotImplementedException();
+
+    public override float Perimeter => throw new NotImplementedException();
+
+    protected override void Draw()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+    }
+}
---
+++ src/MathCalculator/Shapes/Rhombus.cs
@@ -0,0 +1,57 @@
+﻿using MathCalculator.Common;
+
+namespace MathCalculator.Shapes;
+
+public class Rhombus : Shape
+{
+    private float side;
+    private float majorAxis;
+    private float minorAxis;
+
+    public Rhombus()
+    {
+        side = 0;
+        majorAxis = 0;
+        minorAxis = 0;
+    }
+
+
+    public float Side
+    {
+        get => side;
+        set => side = value;
+    }
+
+    public float MajorAxis
+    {
+        get => majorAxis;
+        set => majorAxis = value;
+    }
+
+    public float MinorAxis
+    {
+        get => minorAxis;
+        set => minorAxis = value;
+    }
+
+    public override float Area => throw new NotImplementedException();
+
+    public override float Perimeter => throw new NotImplementedException();
+
+
+    protected override void Draw()
+    {
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            side = 0;
+            majorAxis = 0;
+            minorAxis = 0;
+        }
+
+        base.Dispose(disposing);
+    }
+}
