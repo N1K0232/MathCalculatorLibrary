096d92542ca5ef32e312fc8d8d0ede9bbb891cda
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-09-13 20:36:45 +0200

    added Solid base class

---
+++ src/MathCalculator/Common/Solid.cs
@@ -0,0 +1,18 @@
+﻿namespace MathCalculator.Common;
+
+public abstract partial class Solid : Shape
+{
+	protected Solid()
+	{
+	}
+
+	protected override void Draw()
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override void Dispose(bool disposing)
+	{
+		base.Dispose(disposing);
+	}
+}
