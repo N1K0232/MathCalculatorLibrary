ea4b2f0ebf39d8cf88eb8e3abf85418a4faa86bb
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-09-22 00:36:03 +0200

    added EmptySolid class

---
+++ src/MathCalculator/Solid.EmptySolid.cs
@@ -0,0 +1,22 @@
+﻿namespace MathCalculator;
+
+public abstract partial class Solid
+{
+    private class EmptySolid : Solid
+    {
+        public EmptySolid()
+        {
+            Name = "Empty Solid";
+        }
+
+        public override float Area => 0;
+        public override float Perimeter => 0;
+
+        public override Solid Clone() => this;
+
+        protected override void Draw(Graphics graphics)
+        {
+            throw new InvalidOperationException("can't draw an empty solid");
+        }
+    }
+}
--- src/MathCalculator/Solid.cs
+++ src/MathCalculator/Solid.cs
@@ -2,8 +2,22 @@
 
 public abstract partial class Solid : Shape
 {
+    public static new readonly Solid Empty = new EmptySolid();
+
     protected Solid()
     {
+    }
+
+
+    public new virtual Solid Clone()
+    {
+        Shape shape = base.Clone();
+        if (shape is Solid solid)
+        {
+            return solid;
+        }
+
+        return null!;
     }
 
     protected override void Draw(Graphics graphics)
