9202e4b6bc8c934d29b94208d8bdda0e90c87ff1
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-09-22 00:36:44 +0200

    removed Exception throw

--- src/MathCalculator/Solid.cs
+++ src/MathCalculator/Solid.cs
@@ -22,7 +22,6 @@ public abstract partial class Solid : Shape
 
     protected override void Draw(Graphics graphics)
     {
-        throw new NotImplementedException();
     }
 
     protected override void Dispose(bool disposing)
